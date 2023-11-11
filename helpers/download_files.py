e5a51361ecd13580141f043fa64d4d00f6638678
Lyxal <36217120+Lyxal@users.noreply.github.com>
2023-11-11 22:01:50 +1100

    Create download_files.py

---
+++ helpers/download_files.py
@@ -0,0 +1,12 @@
+import sys
+import requests
+
+if __name__ == "__main__":
+  files = sys.argv[1].split(" !! ")
+  directory = sys.argv[2]
+
+  for file in files:
+    res = requests.get("https://vyxal.github.io/Vyxal/" + file)
+    javascript = res.text
+    with open(directory + "/src/" + file, "w") as file:
+      file.write(javascript)
