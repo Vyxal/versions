873239d3d92ba7e5f81574ae18c278794a279630
Lyxal <36217120+Lyxal@users.noreply.github.com>
2023-11-11 22:47:11 +1100

    Update download_files.py

--- helpers/download_files.py
+++ helpers/download_files.py
@@ -8,5 +8,5 @@ if __name__ == "__main__":
   for file in files:
     res = requests.get("https://vyxal.github.io/Vyxal/" + file)
     javascript = res.text
-    with open(directory + "/src/" + file, "w") as file:
+    with open(directory + file, "w") as file:
       file.write(javascript)
