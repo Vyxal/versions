82a6a511837b83a715b001d5b719620ed0a3dd2d
Lyxal <36217120+Lyxal@users.noreply.github.com>
2023-11-16 13:59:35 +1100

    Create update_versions_files.py

---
+++ helpers/update_versions_files.py
@@ -0,0 +1,10 @@
+import sys
+import json
+
+if __name__ == "__main__":
+  version_tag = sys.argv[1]
+  with open("versions.json", "r") as READ:
+    version_json = json.loads(READ)
+    version_json["versions"].append(version_tag.replace("v", ""))
+  with open("versions.json", "w") as WRITE:
+    WRITE.write(json.dumps(version_json))
