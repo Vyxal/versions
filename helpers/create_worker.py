ba5654a54359d4060db16331b10bfa2e986cc695
Lyxal <36217120+Lyxal@users.noreply.github.com>
2023-11-16 14:03:37 +1100

    Update create_worker.py

--- helpers/create_worker.py
+++ helpers/create_worker.py
@@ -1,20 +1,19 @@
 import sys
 
-TEMPLATE_STRING = """import { Vyxal } from "./vyxal.js";
+TEMPLATE_STRING = """import { Vyxal } from "./vyxal.js"
 
-
-self.addEventListener("message", function (e) {
-  const data = e.data;
-  console.log("Worker received: " + data.mode);
-  const session = data.session;
-  const sendFn = (x) => {
-    this.postMessage({ val: x, command: "append", session: session });
-  };
-  Vyxal.setShortDict(data.dictionary.short);
-  Vyxal.setLongDict(data.dictionary.long);
-  Vyxal.execute(data.code, data.inputs, data.flags, sendFn);
-  this.postMessage({ command: "done", session: data.session });
-});
+self.addEventListener('message', function (e) {
+    var data = e.data;
+    console.log("Worker received: " + data.mode);
+    const session = data.session;
+    const sendFn = x => {
+        this.postMessage({ "val": x, "command": "append", "session": session })
+    };
+    Vyxal.setShortDict(data.shortDict)
+    Vyxal.setLongDict(data.longDict)
+    Vyxal.execute(data.code, data.inputs, data.flags, sendFn)
+    this.postMessage({ "command": "done", "session": data.session })
+})
 """
 
 if __name__ == "__main__":
